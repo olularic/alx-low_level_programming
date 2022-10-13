d77faab2d6c0707da06826d680dec7dab6551829
“olularic” <olularic@gmail.com>
2022-10-13 07:16:17 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Prints a text
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of
-art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
