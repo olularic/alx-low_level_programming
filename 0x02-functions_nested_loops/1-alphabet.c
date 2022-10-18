ea7eb89d78339314ef4335d803180d57286a192c
“olularic” <olularic@gmail.com>
2022-10-18 08:10:27 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* _isdigit-checks for a digit (0 through 9)
-* @c: int to be checked
-* Return: 1 if c is a digit, 0 otherwise
+* print_alphabet - print the alphabet in lowercase followed by a new line
 */
-int _isdigit(int c)
+void print_alphabet(void)
 {
-return (c >= '0' && c <= '9');
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
 }
