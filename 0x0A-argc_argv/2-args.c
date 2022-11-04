ec52a1ac9df58f9d7f020782b4ce0a3d184c3942
“olularic” <olularic@gmail.com>
2022-11-04 15:21:13 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * main - Print the name of the program
 * @argc: Count arguments
@@ -7,10 +8,13 @@
 *
 * Return: Always 0 (Success)
 */
+
 int main(int argc, char *argv[])
 {
+
 /*Declaring variables*/
 int count = 0;
+
 if (argc > 0)
 {
 /*WHILE - Print each arguments*/
