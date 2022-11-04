8781cbf8de8b2c7df76714b0442a2e2e72213e8d
“olularic” <olularic@gmail.com>
2022-11-04 11:22:30 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * main - print the name of the program
 * @argc: Count arguments
@@ -7,9 +8,11 @@
 *
 * Return: Always 0 (Success)
 */
-int main(int argc, char *argv[]
+
+int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
 printf("%i\n", argc - 1);
+
 return (0);
 }
